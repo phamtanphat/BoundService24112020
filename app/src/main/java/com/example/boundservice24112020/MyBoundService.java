6dd23b8f5f570241f1585048f550da331968a764
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-02-27 18:07:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/boundservice24112020/MyBoundService.java
@@ -0,0 +1,86 @@
+package com.example.boundservice24112020;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public class MyBoundService extends Service {
+
+    int count = 0;
+    NotificationManager mNotificationManager;
+    NotificationCompat.Builder mBuilder;
+    String CHANNEL_ID = "MY_CHANNEL";
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mBuilder = createNotification();
+        startForeground(1 , mBuilder.build());
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                count++;
+                new Handler().postDelayed(this , 1000);
+            }
+        },1000);
+        return super.onStartCommand(intent, flags, startId);
+    }
+    private NotificationCompat.Builder createNotification(){
+        Intent intent = new Intent(this , MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getService(
+                this,
+                123,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        NotificationCompat.Builder notify = new NotificationCompat.Builder(MyBoundService.this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.btn_star)
+                .setShowWhen(true)
+                .setContentTitle("Thông báo mới")
+                .setContentText("Có phiên bản mới")
+                .addAction(R.mipmap.ic_launcher , "Open App" ,pendingIntent)
+                .setPriority(2);
+
+//                .setStyle(new Notification.MediaStyle().setShowActionsInCompactView(0 , 1).setMediaSession())
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Notification",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .build();
+            notificationChannel.enableVibration(true);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+        return notify;
+
+    }
+}
